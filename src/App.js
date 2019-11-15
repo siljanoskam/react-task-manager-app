208c6247fc6ecd90b6532f167a8b1d0bb8c7eb67
Magdalena Siljanoska <magdalenasiljanoska979@gmail.com>
2019-11-15 16:03:29 +0100

    Add editorconfig

--- src/App.js
+++ src/App.js
@@ -6,98 +6,99 @@ const userId = 1; // We'll be using a const ID for the data fetches (for testing
 const isTodoCompleted = false; // The Todos will be uncompleted by default
 
 class TaskApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [],
-            task: ''
-        };
-
-        this.onInputChange = this.onInputChange.bind(this);
-        this.addTask = this.addTask.bind(this);
-        this.fetchTaskAdd = this.fetchTaskAdd.bind(this);
-    }
-
-    componentDidMount() {
-        this.fetchTasks();
-    }
-
-    fetchTasks() {
-        return fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
-            .then(response => response.json())
-            .then((tasks) => {
-                this.setState({items: tasks})
-            })
-            .catch(console.log);
-    }
-
-    fetchTaskAdd() {
-        return fetch('https://jsonplaceholder.typicode.com/todos', {
-            method: 'POST',
-            body: JSON.stringify({
-                userId: userId,
-                title: this.state.task,
-                completed: isTodoCompleted
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        })
-            .then(response => response.json())
-            .then((task) => {
-                this.setState({
-                    items: this.state.items.concat(task)
-                });
-            })
-            .catch(console.log);
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      task: ''
     };
 
-    addTask(event) {
-        event.preventDefault();
+    this.onInputChange = this.onInputChange.bind(this);
+    this.addTask = this.addTask.bind(this);
+    this.fetchTaskAdd = this.fetchTaskAdd.bind(this);
+  }
 
-        const taskInput = document.getElementById('task-input');
+  componentDidMount() {
+    this.fetchTasks();
+  }
 
-        if (this.state.task) {
-            this.fetchTaskAdd();
+  fetchTasks() {
+    return fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
+      .then(response => response.json())
+      .then((tasks) => {
+        this.setState({items: tasks})
+      })
+      .catch(console.log);
+  }
 
-            this.setState({
-                task: ''
-            });
-        } else {
-            alert('The task cannot be empty. Please enter a task.')
-        }
+  fetchTaskAdd() {
+    return fetch('https://jsonplaceholder.typicode.com/todos', {
+      method: 'POST',
+      body: JSON.stringify({
+        userId: userId,
+        title: this.state.task,
+        completed: isTodoCompleted
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(response => response.json())
+      .then((task) => {
+        this.setState({
+          items: this.state.items.concat(task)
+        });
+      })
+      .catch(console.log);
+  };
 
-        taskInput.focus();
+  addTask(event) {
+    event.preventDefault();
+
+    const taskInput = document.getElementById('task-input');
+
+    if (this.state.task) {
+      this.fetchTaskAdd();
+
+      this.setState({
+        task: ''
+      });
+    } else {
+      alert('The task cannot be empty. Please enter a task.')
     }
 
-    onInputChange(event) {
-        this.setState({task: event.target.value})
-    }
+    taskInput.focus();
+  }
 
-    render() {
-        return (
-            <div className="task-app container-fluid">
-                <div className="container p-5">
-                    <h1>My tasks</h1>
+  onInputChange(event) {
+    this.setState({task: event.target.value})
+  }
 
-                    <form className="form-inline mb-5 mt-5" onSubmit={this.addTask}>
-                        <input
-                            id="task-input"
-                            placeholder="Enter your task here..."
-                            className="form-control"
-                            onChange={this.onInputChange}
-                            value={this.state.task}
-                        />
-                        <button className="btn btn-info ml-1 font-weight-bold">
-                            Add Task
-                        </button>
-                    </form>
+  render() {
+    return (
+      <div className="task-app container-fluid">
+        <div className="container p-5">
+          <h1>My tasks</h1>
 
-                    <TaskList items={this.state.items}/>
-                </div>
-            </div>
-        );
-    }
+          <form className="form-inline mb-5 mt-5" onSubmit={this.addTask}>
+            <input
+              id="task-input"
+              placeholder="Enter your task here..."
+              className="form-control"
+              onChange={this.onInputChange}
+              value={this.state.task}
+              autoComplete="off"
+            />
+            <button className="btn btn-info ml-1 font-weight-bold">
+              Add Task
+            </button>
+          </form>
+
+          <TaskList items={this.state.items}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TaskApp;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,38 +1,38 @@
 import React from 'react';
 
 class TaskList extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.displayTask = this.displayTask.bind(this);
-    }
+    this.displayTask = this.displayTask.bind(this);
+  }
 
-    displayTask(task) {
-        return <li
-            className={`font-weight-bold ${task.completed ? 'strikethrough' : ''}`}
-            key={`${task.id}`}
-            onClick={this.finishTask}
-            title="Click to finish this task"
-        >
-            {task.title}
-        </li>
-    };
+  displayTask(task) {
+    return <li
+      className={`font-weight-bold ${task.completed ? 'strikethrough' : ''}`}
+      key={`${task.id}`}
+      onClick={this.finishTask}
+      title="Click to finish this task"
+    >
+      {task.title}
+    </li>
+  };
 
-    finishTask(event) {
-        event.target.classList.add('strikethrough');
-    };
+  finishTask(event) {
+    event.target.classList.add('strikethrough');
+  };
 
-    render() {
-        return <div className="card card-body bg-info mb-2">
-            <ul className="task-list text-white">
-                {
-                    this.props.items.length !== 0 ?
-                        this.props.items.map(this.displayTask) :
-                        <label className="font-weight-bold">You have no tasks at the moment!</label>
-                }
-            </ul>
-        </div>
-    }
+  render() {
+    return <div className="card card-body bg-info mb-2">
+      <ul className="task-list text-white">
+        {
+          this.props.items.length !== 0 ?
+            this.props.items.map(this.displayTask) :
+            <label className="font-weight-bold">You have no tasks at the moment!</label>
+        }
+      </ul>
+    </div>
+  }
 }
 
 export default TaskList;
