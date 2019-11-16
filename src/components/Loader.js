0c75cb4a6b00b4842c33d9c101a753737aad4f29
Magdalena Siljanoska <magdalenasiljanoska979@gmail.com>
2019-11-16 21:18:20 +0100

    Remove unnecessary constructor on Loader component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 class Loader extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return <div id="loader" className={`float-right ${this.props.isLoaderHidden ? 'd-none' : ''}`}>
       <div className="lds-roller">
